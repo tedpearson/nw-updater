fac79cb2367b47dc6fe92e9e05d1dfa38670a578
Ted Pearson <1477390+tedpearson@users.noreply.github.com>
2023-10-24 00:10:40 -0400

    add ability to answer secret questions for goigoe
    questions are not required for other institutions

--- institution/common.go
+++ institution/common.go
@@ -24,6 +24,7 @@ import (
 type Auth struct {
 	Username          string
 	EncryptedPassword string `yaml:"encrypted_password"`
+	Questions         map[string]string
 }
 
 // AccountMapping contains the account name in the institution and the mapping to the YNAB account name.
--- institution/igoe.go
+++ institution/igoe.go
@@ -2,6 +2,7 @@ package institution
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
@@ -22,7 +23,7 @@ type igoe struct {
 func (i igoe) GetBalances(ctx context.Context, auth Auth, d decrypt.Decryptor, mapping []AccountMapping) (map[string]int64, error) {
 	ctx, cancel := newContext(ctx, igoeUrlPrefix)
 	defer cancel()
-	err := i.auth(ctx, auth.Username, d.Decrypt(auth.EncryptedPassword))
+	err := i.auth(ctx, auth, d)
 	if err != nil {
 		return nil, err
 	}
@@ -33,12 +34,42 @@ func (i igoe) GetBalances(ctx context.Context, auth Auth, d decrypt.Decryptor, m
 	}, ctx, mapping, ".b-dashboard-accounts-name", ".b-dashboard-accounts-balance .currency-span")
 }
 
-func (i igoe) auth(ctx context.Context, username, password string) error {
-	return chromedp.Run(ctx,
+func (i igoe) auth(ctx context.Context, auth Auth, d decrypt.Decryptor) error {
+
+	var questions []*cdp.Node
+	err := chromedp.Run(ctx,
 		chromedp.Navigate(igoeLoginUrl),
-		chromedp.SetValue("#un", username, chromedp.ByQuery),
-		chromedp.SetValue("#password", password, chromedp.ByQuery),
+		chromedp.SetValue("#un", auth.Username, chromedp.ByQuery),
+		chromedp.SetValue("#password", d.Decrypt(auth.EncryptedPassword), chromedp.ByQuery),
 		chromedp.Click(".button-subm", chromedp.ByQuery),
-		chromedp.WaitVisible("accounts-summary-mini", chromedp.ByQuery),
+		chromedp.WaitVisible("accounts-summary-mini,div.secq-form", chromedp.ByQuery),
+		chromedp.Nodes("div.secq-form div.field-label label", &questions, chromedp.ByQueryAll, chromedp.AtLeast(0)),
 	)
+	if err != nil {
+		return err
+	}
+	if len(questions) == 0 {
+		return nil
+	}
+	for _, node := range questions {
+		var question string
+		err := chromedp.Run(ctx,
+			chromedp.TextContent([]cdp.NodeID{node.NodeID}, &question, chromedp.ByNodeID))
+		if err != nil {
+			return err
+		}
+		answer, ok := auth.Questions[question]
+		if !ok {
+			return fmt.Errorf("did not have answer to question: %s", question)
+		}
+		id := node.AttributeValue("for")
+		err = chromedp.Run(ctx,
+			chromedp.SetValue(fmt.Sprintf("#%s", id), answer, chromedp.ByQuery))
+		if err != nil {
+			return err
+		}
+	}
+	return chromedp.Run(ctx,
+		chromedp.Click(".button-subm", chromedp.ByQuery),
+		chromedp.WaitVisible("accounts-summary-mini", chromedp.ByQuery))
 }
